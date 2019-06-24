da40db0e7c8109293de580da156a9e32f49509a5
a1685823 <a1685823@student.adelaide.edu.au>
2019-06-24 22:15:09 +0930

    Grabs samba state, needs to check time relevance of entry.

--- smbd_monitor.py
+++ smbd_monitor.py
@@ -1,24 +1,42 @@
 import os
 import smtplib, ssl
+from email.message import EmailMessage
 import subprocess
 
 # grab status of samba server from last in log
-smbstatus = subprocess.check_output(['service', 'smbd', 'status'])
-smbstatus = smbstatus.split('\n')[-2]
-state = smbstatus.find('Started', beg=0, end=len(string))
+def samba_state():
+    samba_status = subprocess.check_output(['service', 'smbd', 'status'])
+    samba_status = str(samba_status.splitlines()[-1])
+    return samba_status.find('Started')
 
-# if found
-if state != -1:
+
+# if running, exit script
+if samba_state() != -1:
     quit()
 
-# else
-sender_email = os.environ.get('SMBD_MONITOR_SEND_EMAIL')
-sender_password = os.environ.get('SMBD_MONITOR_SEND_PASSWORD')
-receiver_email = os.environ.get('SMBD_MONITOR_RECEIVE_EMAIL')
+# else restart up to five times unless unsucessful
+else:
 
-port = 465
-context = ssl.create_default_context()
+    for i in range(1, 5):
+        subprocess.check_output(['service', 'smbd', 'status'])
 
-with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
-    server.login(sender_email, sender_password)
-    # TODO: Send email here
+        if samba_state() == -1:
+            quit()
+
+    # else email to prompt fix
+    ############# need to check if email has been recently sent or problem fixed
+    sender_email = os.environ.get('SMBD_MONITOR_SEND_EMAIL')
+    sender_password = os.environ.get('SMBD_MONITOR_SEND_PASSWORD')
+    receiver_email = os.environ.get('SMBD_MONITOR_RECEIVE_EMAIL')
+
+    port = 465
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+        server.login(sender_email, sender_password)
+        msg = EmailMessage()
+        msg.set_content('')
+        msg['Subject'] = 'Server down: Samba'
+        msg['From'] = sender_email
+        msg['To'] = receiver_email
+        server.sendmail(sender_email, receiver_email, msg)
